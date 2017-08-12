b1a365c3d79f8b9e9c7097516dcaee187d64b69e
oikiou <oikiou@yahoo.co.jp>
2017-08-12 21:15:46 +0900

    Update duct_balance.py

--- duct_balance.py
+++ duct_balance.py
@@ -29,8 +29,13 @@ import random
 # 管长，管径，管道粗糙度默认相同， S = 3.125 * 10 ^ -6
 
 k = 16000
-se = 0.000001875
-s = 0.000003125
+se = 1.9 / 3600 / 3600
+s1 = 2.2 / 3600 / 3600
+s2 = 2.2 / 3600 / 3600
+s3 = 2.2 / 3600 / 3600
+s4 = 5.8 / 3600 / 3600
+s5 = 1.3 / 3600 / 3600
+s6 = 2.1 / 3600 / 3600
 
 
 # ## 风机特性
@@ -50,6 +55,8 @@ def performance_characteristics(inv, x1, y1, x2, y2, x3, y3):
 fan_1_performance = performance_characteristics(1, 2000, 200, 4000, 150, 6000, 70)
 # 2号风机的特性曲线
 fan_2_performance = performance_characteristics(1, 6000, 600, 12000, 400, 18000, 210)
+# 3号风机的特性曲线
+fan_3_performance = performance_characteristics(2, 6000, 170, 12000, 150, 18000, 80)
 
 # 利用风机特性曲线，求压力
 def g_to_p_fan(g, inv, performance):
@@ -57,7 +64,7 @@ def g_to_p_fan(g, inv, performance):
     return (g1 * performance * inv).tolist()[0][0]
 
 # 特性曲线图
-x_pump1 = np.linspace(0, 6000, 50)
+x_pump1 = np.linspace(0, 18000, 50)
 y_pump1 = []
 for i in range(50):
     y_pump1.append(g_to_p_fan(x_pump1[i], 1, fan_1_performance))
@@ -86,49 +93,47 @@ def cal_p0_to_g2(p0, s, se, cv, k):
     return math.sqrt((2 * p0 * s + se * p0 + 1 / cv / cv / k - math.sqrt((2 * p0 * s + se * p0 + 1 / cv / cv / k) *
          (2 * p0 * s + se * p0 + 1 / cv / cv / k) - 4 * (s * s + s * se) * p0 * p0))/(2 * (s * s + s * se)))
 
-for i in range(30000):
+for i in range(60000):
     # 随机取初值
     cv1 = random.random()
     cv2 = random.random()
     cv3 = random.random()
 
     # 假定一个g1
-    g1 = random.random() * 6000
+    g1 = random.random() * 4000
     #g1 = cv1 * 6000 + (random.random() - 0.5) * 600
-
-    if g1 < 0:
-        continue
 
     # 计算流程 g1 - p1 - p2 - g2 - p3 - g3 - g - p
     # g1 - p1
     p1 = cal_g1_to_p1(g1, se, cv1, k)
 
     # p1 - p2
-    p2 = p1 + 2 * s * g1 * g1
+    p2 = p1 + s1 * g1 * g1 + s4 * g1 * g1
 
     # p2 - g2
-    g2 = cal_p0_to_g2(p2, s, se, cv2, k)
+    g2 = cal_p0_to_g2(p2, s2, se, cv2, k)
 
     # g2 - p3
-    p3 = p2 + 2 * s * (g1 + g2) * (g1 + g2)
+    p3 = p2 + s5 * (g1 + g2) * (g1 + g2)
 
     # p3 - g3
-    g3 = cal_p0_to_g2(p3, s, se, cv3, k)
+    g3 = cal_p0_to_g2(p3, s3, se, cv3, k)
 
-    if g3 > 6000:
+    if g2 > 4000 or g3 > 4000:
         continue
 
     # g3 - p
-    p = p3 + 2 * s * (g1 + g2 + g3) * (g1 + g2 + g3)
-
-    if p > 600:
-        continue
+    p = p3 + s6 * (g1 + g2 + g3) * (g1 + g2 + g3)
 
     # p - inv
-    pi1 = g_to_p_fan((g1 + g2 + g3), 1, fan_2_performance)
+    pi1 = g_to_p_fan((g1 + g2 + g3), 1, fan_3_performance)
     inv = p / pi1
 
-    if inv > 1 or inv < 0.1:
+    if inv > 1 or inv < 0.2:
+        continue
+
+    drop_p = random.random()
+    if inv * inv < drop_p:
         continue
 
     print(cv1, cv2, cv3, g1, g2, g3, g1 + g2 + g3, p, inv)
